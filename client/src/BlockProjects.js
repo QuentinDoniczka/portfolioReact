cff9b4c1f0122ffef8518e29712360245379c6fa
QuentinDoniczka <doniczka.quentin67@gmail.com>
2023-04-02 17:55:46 +0200

    bon

--- client/src/BlockProjects.js
+++ client/src/BlockProjects.js
@@ -3,7 +3,6 @@ import { CardProject } from './CardProject';
 import { Container } from 'react-bootstrap';
 
 export const BlockProjects = ({ projects }) => {
-    console.log(projects);
     return (
             <Container className="d-flex justify-content-center">
             {projects.slice(0, 3).map(project => (
