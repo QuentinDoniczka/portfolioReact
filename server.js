6a3c27f510e5d3f83f17426f535dca2f226fe743
QuentinDoniczka <doniczka.quentin67@gmail.com>
2023-04-02 18:15:29 +0200

    express

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started on port ${process.env.PORT || 5000}`);
+});
