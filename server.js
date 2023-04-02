97437623663e775abf859738f717ec53c81165bc
QuentinDoniczka <doniczka.quentin67@gmail.com>
2023-04-02 18:23:18 +0200

    final

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Servir les fichiers statiques à partir du répertoire de build
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Les requêtes qui ne correspondent pas aux fichiers statiques doivent être renvoyées à l'application React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server started on port ${process.env.PORT || 5000}`);
+// Lancer le serveur sur le port spécifié ou sur le port 5000 par défaut
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
