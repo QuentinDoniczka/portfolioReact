5ef8c52134fb38b782b7aefe92ccbd6c5a1dbe45
QuentinDoniczka <doniczka.quentin67@gmail.com>
2023-04-12 12:24:20 +0200

    v1.0.0

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,20 +6,17 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='d-flex align-items-center'>
+          <Col md={9} className='d-flex align-items-center'>
             <h4>© Quentin Doniczka</h4>
           </Col>
-          <Col className="d-flex flex-column align-items-center">
-            <h5>Contact</h5>
-            <p className="d-flex flex-column align-items-start ">
+          <Col md={3} className="d-flex flex-column align-items-center">
+            <h5 className='p-1'>Contact</h5>
+            <p className="d-flex flex-column align-items-start">
               <span>Email: <a href="mailto:doniczka.quentin67@gmail.com">doniczka.quentin67@gmail.com</a></span>
               <span>Téléphone: 06 12 93 86 28</span>
             </p>
           </Col>
         </Row>
-      </Container>
-      <Container>
-        
       </Container>
     </footer>
   );
