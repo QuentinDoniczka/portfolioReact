696e6991236712f989fdfd9e4792b6498fed7bfa
QuentinDoniczka <doniczka.quentin67@gmail.com>
2023-05-20 17:58:46 +0200

    video

--- client/src/imageMapping.js
+++ client/src/imageMapping.js
@@ -1,11 +1,13 @@
 import siteRecette from './assets/siteRecette.png';
 import sqliteNode from './assets/sqliteNode.png';
 import portfolio from './assets/portfolio.png';
+import geometryDash from './assets/geometryDash.png';
 
 const imageMapping = {
   'siteRecette.png': siteRecette,
   'sqliteNode.png': sqliteNode,
   'portfolio.png' : portfolio,
+  'geometryDash.png' : geometryDash,
 };
 
 export default imageMapping;
--- client/src/pages/ProjectDetails.js
+++ client/src/pages/ProjectDetails.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Button, Container } from 'react-bootstrap';
 
 const ProjectDetails = ({ projects }) => {
+  const [showVideo, setShowVideo] = useState(false);
   const { id } = useParams();
   const project = projects.find((p) => p.id === parseInt(id, 10));
 
@@ -20,22 +21,52 @@ const ProjectDetails = ({ projects }) => {
     ));
   };
 
+  const toggleVideo = () => {
+    setShowVideo(!showVideo);
+  };
+
+  const renderVideo = () => {
+    if (showVideo && project.linkVideo && project.linkVideo.trim() !== '') {
+      let videoId;
+      if (project.linkVideo.includes('youtu.be')) {
+        videoId = project.linkVideo.split('youtu.be/')[1];
+      } else {
+        videoId = new URL(project.linkVideo).searchParams.get('v');
+      }
+      return (
+        <div style={{ width: '80%', height: '60vh', position: 'relative', margin: '0 auto' }}>
+          <iframe
+            style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <Container className="project-details-container">
-        <h1 className='text-center'>{project.title}</h1>
+      <h1 className='text-center'>{project.title}</h1>
       <Container className="project-details-font">
         <div className="mx-auto">
           <p>
             {renderLongDescription()}
           </p>
+          {renderVideo()}
         </div>
       </Container>
       <Container className="d-flex justify-content-center align-items-center project-links gap-5 p-5">
-          <Button href={project.linkGit} target="_blank">GitHub</Button>
-          <Button href={project.linkProject} target="_blank">Site du Projet</Button>
-        </Container>
+        <Button href={project.linkGit} target="_blank">GitHub</Button>
+        <Button href={project.linkProject} target="_blank">Site du Projet</Button>
+        {project.linkVideo && project.linkVideo.trim() !== '' && (
+    <Button onClick={toggleVideo}>{showVideo ? 'Masquer la Vidéo' : 'Afficher la Vidéo'}</Button>
+  )}
+      </Container>
     </Container>
-
   );
 };
 
